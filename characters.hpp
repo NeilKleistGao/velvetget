2fb8cbd35c754debcdec520aa5356ae5fe864bdc
NeilKleistGao <neilkleist@gmail.com>
2025-08-04 09:46:03 +0800

    Import cxxopts

---
+++ characters.hpp
@@ -0,0 +1,12 @@
+#ifndef __VELVET_CHARACTERS_H__
+#define __VELVET_CHARACTERS_H__
+
+#include <array>
+
+const static std::array<std::string, 2> CHARACTERS {{
+  "igoru",
+  "erizabesu"
+}};
+
+#endif // __VELVET_CHARACTERS_H__
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cxxopts.hpp>
+#include <characters.hpp>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+int main(int argc, char* argv[]) {
+  cxxopts::Options options("velvetget",
+                           "show velvet assistant in your terminal");
+  options.add_options()
+    ("c,character", "Character", cxxopts::value<std::string>());
+
+  auto result = options.parse(argc, argv);
+  if (result.count("character")) {
+    std::cout << "welcome!" << std::endl; // TODO: read from text file
+  }
+  else {
+    // TODO: random
+  }
+
   return 0;
 }
